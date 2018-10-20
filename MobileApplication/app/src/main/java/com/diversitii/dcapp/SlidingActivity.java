734c2791799d92a1b6c694a074ca5d11c4beba61
MissBeanF <missbean@mail.com>
2018-10-20 19:16:37 +0800

    Sliding Panel is added again

---
+++ MobileApplication/app/src/main/java/com/diversitii/dcapp/SlidingActivity.java
@@ -0,0 +1,26 @@
+package com.diversitii.dcapp;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class SlidingActivity extends MusicActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        boolean mIsPortrait = Utils.isPortrait(this);
+        boolean mIsAndroidTV = Utils.isAndroidTV(this);
+        if (mIsAndroidTV) {
+            //Toast.makeText(this, "This is Android TV", Toast.LENGTH_LONG).show();
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            setContentView(R.layout.sliding_panel);
+        } else {
+            if (Utils.isPortrait(this)) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                //Toast.makeText(this,"This is Tablet",Toast.LENGTH_LONG).show();
+            }
+            setContentView(R.layout.sliding_panel);
+        }
+
+    }
+}
